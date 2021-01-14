4850c8b5370a60f927e5efe7005d69d37ad55325
Riccardo Foti <riccardofoti97@gmail.com>
2021-01-14 12:40:18 +0000

    add removeEventListener

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-document.addEventListener('click', function() {
+function myEventListenerFunction() {
     const mainHeading = document.querySelector('h1');
     mainHeading.style.backgroundColor = 'red';
-})
+}
+document.addEventListener('click', myEventListenerFunction);
+
+//document.removeEventListener('click', myEventListenerFunction);
